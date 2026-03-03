f023a8467742b31971e0e88c0389fe3a2c006a3c
khadri2021 <khadri.institutions@gmail.com>
2026-03-03 22:26:26 +0530

    updated for task : register student in college

---
+++ src/main/java/org/khadri/spring/core/driver/DriverCollege.java
@@ -0,0 +1,17 @@
+package org.khadri.spring.core.driver;
+
+public class DriverCollege {
+    public static void main(String[] args) {
+
+
+        // Module Register Student in College
+        // Use do-while
+        // students to register in College
+        // Develop Student class
+        // Develop College class
+        // Inputs from console for the student to register in college.
+        // Register Student in BTech for every branch [CSE,IT,EEE,ECE]
+
+
+    }
+}
