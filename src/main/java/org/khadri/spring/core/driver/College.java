a6e1a061a96d1667fcca49e312a279bfc278e6fe
Lakshmi <alreddy02@gmail.com>
2026-03-06 21:32:37 +0530

    Modified files

---
+++ src/main/java/org/khadri/spring/core/driver/College.java
@@ -0,0 +1,7 @@
+package org.khadri.spring.core.driver;
+
+public class College {
+    public void registerStudent(Student student) {
+        student.display();
+    }
+}
--- src/main/java/org/khadri/spring/core/driver/DriverCollege.java
+++ src/main/java/org/khadri/spring/core/driver/DriverCollege.java
@@ -1,17 +1,69 @@
 package org.khadri.spring.core.driver;
 
+import java.util.Scanner;
+import java.util.Arrays;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class DriverCollege {
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-        // Module Register Student in College [Use spring container concept]
-        // Use do-while
-        // students to register in College
-        // Develop Student class
-        // Develop College class
-        // Inputs from console for the student to register in college.
-        // Register Student in BTech for every branch [CSE,IT,EEE,ECE]
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("beans.xml");
 
+        College college = context.getBean("college", College.class);
 
+        boolean decision = true;
+
+        do {
+
+            System.out.println("Please select operation type: " + Arrays.asList("REGISTER", "EXIT"));
+
+            String operationType = sc.next();
+
+            switch (operationType) {
+
+                case "REGISTER": {
+
+                    System.out.println("REGISTER Operation Starts");
+
+                    System.out.println("Enter Student ID:");
+                    int id = sc.nextInt();
+                    sc.nextLine();
+
+                    System.out.println("Enter Student Name:");
+                    String name = sc.nextLine();
+
+                    System.out.println("Enter Branch (CSE/IT/EEE/ECE):");
+                    String branch = sc.nextLine();
+
+                    Student student = new Student();
+                    student.setId(id);
+                    student.setName(name);
+                    student.setBranch(branch);
+
+                    college.registerStudent(student);
+
+                    System.out.println("REGISTER Operation Ends");
+                    break;
+                }
+
+                case "EXIT": {
+                    decision = false;
+                    System.out.println("Exiting Application...");
+                    break;
+                }
+
+                default: {
+                    System.out.println("Invalid Operation!");
+                }
+            }
+
+        } while (decision);
+
+        sc.close();
     }
 }
